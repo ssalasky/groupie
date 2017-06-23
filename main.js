38e17e6fbfaf1f4bba8b3fe58dfe40b478be490a
adcarey <Adam@Adams-MacBook.local>
2017-06-22 20:16:01 -0600

    test

--- main.js
+++ main.js
@@ -1,12 +1,10 @@
-<<<<<<< HEAD
-<<<<<<< HEAD
+
 
 var destCity = "";
 var yelpURL = "https://api.yelp.com/v3/businesses/search?location=" + city;
 var city = "";
 
 
-alert("connected");
 
 
 var flights = {
@@ -41,42 +39,4 @@ function yelpSearch() {
 }
 
 
-=======
-=======
 
->>>>>>> 2cc0ea047717fa5e332901f38790cf5ff1885019
-var destCity = ""; 
-var yelpURL = "https://api.yelp.com/v3/businesses/search?location=" + city; 
-var city = "";    
-
-var flights = {
-     to:"",
-     from:"",     
-     departTo:"",     
-     departHome:"" 
- }
-
-function flightSearch(){
-     $("#glyph").on("click", function(){
-        var to = $("#search-input").val().trim();
-        var queryURL = "https://api.test.sabre.com/v1/shop/flights?origin=JFK&destination=" + to + "&departuredate=2017-07-07&returndate=2017-07-08&onlineitinerariesonly=N&limit=10&offset=1&eticketsonly=N&sortby=totalfare&order=asc&sortby2=departuretime&order2=asc&pointofsalecountry=US";
-
-        $.ajax({
-            url: queryURL,         
-            method: "GET"     
-        }).done((response) => {
-            console.log(response);     
-        }); 
-    }); 
- }; 
-flightSearch();
-     
-function yelpSearch() {     
-	$.ajax ({         
-		url: yelpURL,         
-		method: "GET"     
-	}).done(function(response) {         
-		console.log(response);     
-	}); 
-}
->>>>>>> b4eba92c75423ab812d8f7376251748e6c41437c
