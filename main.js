9edd3d5170119d3472574e43d488b694f89daeee
adcarey <Adam@Adams-MacBook.local>
2017-06-22 19:37:27 -0600

    changes

--- main.js
+++ main.js
@@ -1,5 +1,12 @@
+<<<<<<< HEAD
+var destCity = "";
+var yelpURL = "https://api.yelp.com/v3/businesses/search?location=" + city;
+var city = "";
+
+=======
 
 alert("connected");
+>>>>>>> edcdad3aef21aa4e2e2dbd887c8609b02287d90d
 
 var flights= {
 	to:"",
@@ -21,3 +28,16 @@ function flightSearch(){
 };
 flightSearch();
 
+<<<<<<< HEAD
+
+function yelpSearch() {
+	$.ajax ({
+		url: yelpURL,
+		method: "GET"
+	}).done(function(response) {
+		console.log(response);
+	});
+}
+
+=======
+>>>>>>> edcdad3aef21aa4e2e2dbd887c8609b02287d90d
