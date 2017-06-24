205b2c6590419324a5c4ddcdb3786240bfde290d
Danny Jimenez <daj777@gmail.com>
2017-06-24 10:50:48 -0600

    Update main.js

--- main.js
+++ main.js
@@ -55,5 +55,29 @@ function yelpSearch() {
 	});
 }
 
+ 	var artist = "";
+ 	var queryURL = "https://api.seatgeek.com/2/events?q=" + artist + "&client_id=MTAyMzg3N3wxNDk4MDEzODgyLjUy";
+
+    
+    $("#submitArtist").on("click", function(){
+    	artist = $("#artistID").val().trim();
+
+    // Javascript validation for search input
+    
+    	if(artist == '') {
+    		return false
+    	}
+
+    	$.ajax({
+      url: queryURL,
+      method: 'GET'
+    }).done(function(response) {
+      console.log(response);
+
+   
+   
+})
+    });
+
 
 
