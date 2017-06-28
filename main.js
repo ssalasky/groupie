0144cc56ba1edf6b8f7c86297717403f5634053d
ssalasky <samsalasky@hotmail.com>
2017-06-27 18:04:59 -0600

    adding places api

--- main.js
+++ main.js
@@ -40,46 +40,45 @@ function flightSearch(){
 
 flightSearch();
 
-
-
-    
-    $("#glyph").on("click", function(){
-    	artist = $("#search-input").val().trim();
-
-    	var queryURL = "https://api.seatgeek.com/2/events?q=" + artist + "&per_page=1&client_id=MTAyMzg3N3wxNDk4MDEzODgyLjUy";
-
-    	console.log(artist);
-
-
-    // Javascript validation for search input
-    
-    	if(artist == '') {
-
-    		return false
-    	}
-
-
-
-
-    	 	var artist = "";
- 	
-
-
   
-    // Javascript validation for search input
-    
-    
+$("#glyph").on("click", function(){
+	artist = $("#search-input").val().trim();
 
-    	$.ajax({
-      url: queryURL,
-      method: 'GET'
+	var queryURL = "https://api.seatgeek.com/2/events?q=" + artist + "&per_page=1&client_id=MTAyMzg3N3wxNDk4MDEzODgyLjUy";
+
+	console.log(artist);
+// Javascript validation for search input
+
+	if(artist == '') {
+		return false
+	}
+
+	var artist = "";
+
+	$.ajax({
+    url: queryURL,
+    method: 'GET'
+  }).done(function(response) {
+    console.log(response);
+    var location = response.events[0].venue.city;
+    var upcomingEvents = response.events[0].has_upcoming_events;
+ 
+  // console.log(location);
+   })
+});
+
+function placeSearch() {
+  // $("#glyph").on("click", function() {
+
+    var placeURL = "https://maps.googleapis.com/maps/api/place/nearbysearch/json?location=-33.8670,151.1957&radius=500&types=food&name=cruise&key=AIzaSyAoBexp2doZWkhqk1nNKby3KfXIa737dMs"
+
+    $.ajax({
+      url: placeURL,
+      method: "GET"
     }).done(function(response) {
       console.log(response);
-      var location = response.events[0].venue.city;
-      var upcomingEvents = response.events[0].has_upcoming_events;
-     
-      // console.log(location);
-   
-   
-})
     });
+  // });
+};
+
+placeSearch();
