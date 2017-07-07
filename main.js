d8640cc4cbeb7751bfcc8a5f2bf115a22578885c
HayelomR <rhayelom@gmail.com>
2017-07-06 20:21:53 -0600

    minor change to index

--- main.js
+++ main.js
@@ -1,45 +1,26 @@
 $("#loadingScreen").hide();
 
 var config = {
-    apiKey: "AIzaSyD7Dl_oVcskvGAUxxgm3LwQC_saHWDZlbQ",
-    authDomain: "groupie-project.firebaseapp.com",
-    databaseURL: "https://groupie-project.firebaseio.com",
-    projectId: "groupie-project",
-    storageBucket: "groupie-project.appspot.com",
-    messagingSenderId: "98043513312"
-  };
- firebase.initializeApp(config);
- var database = firebase.database();
+  apiKey: "AIzaSyD7Dl_oVcskvGAUxxgm3LwQC_saHWDZlbQ",
+  authDomain: "groupie-project.firebaseapp.com",
+  databaseURL: "https://groupie-project.firebaseio.com",
+  projectId: "groupie-project",
+  storageBucket: "groupie-project.appspot.com",
+  messagingSenderId: "98043513312"
+};
+
+firebase.initializeApp(config);
+
+var database = firebase.database();
+
 $("#search-button").on("click", function(){
-    event.preventDefault();
-    artist = $("#search-input").val();
-//push to the database
+  event.preventDefault();
+  artist = $("#search-input").val();
+
   database.ref().push({
     artist:artist
   });
 });
-$("#loadingScreen").hide();
-
-var config = {
-    apiKey: "AIzaSyD7Dl_oVcskvGAUxxgm3LwQC_saHWDZlbQ",
-    authDomain: "groupie-project.firebaseapp.com",
-    databaseURL: "https://groupie-project.firebaseio.com",
-    projectId: "groupie-project",
-    storageBucket: "groupie-project.appspot.com",
-    messagingSenderId: "98043513312"
-  };
- firebase.initializeApp(config);
- var database = firebase.database();
-$("#search-button").on("click", function(){
-    event.preventDefault();
-    artist = $("#search-input").val();
-//push to the database
-  database.ref().push({
-    artist:artist
-  });
-});
-
-
 
 var t;
 var artist = "";
@@ -47,20 +28,22 @@ var hotelArea = "";
 var venueName = "";
 var areaLocation = "";
 var website = "";
-//var airCode ="";
+var venueLongitude = "";
+var venueLatitude = "";
+
+
 var params = {
   request: {
     slice: [
     {
-      origin: "",
-
+      origin: "DEN",
       destination: "",
       date: "",
       maxStops: 1
     },
-     {
+    {
       origin: "",
-      destination: "",
+      destination: "DEN",
       date: "",
       maxStops: 1
     }
@@ -79,27 +62,29 @@ var params = {
 
 
 function startSearch(){
-    $("#first-page").empty();
-    loading();
-//     flightSearch();
-//     // here we will call the function that are needed.
-  };
-  
-// }
-
+  $("#first-page").empty();
+  loading();
+};
 
 function flightSearch(){
-        var queryURL = "https://cors-anywhere.herokuapp.com/https://www.googleapis.com/qpxExpress/v1/trips/search?key=AIzaSyAoBexp2doZWkhqk1nNKby3KfXIa737dMs"//AIzaSyDlW31JmWnRfy96JfYhjDQiL2ZQNYB2xkk";
 
 
 
-        var queryURL = "https://cors-anywhere.herokuapp.com/https://www.googleapis.com/qpxExpress/v1/trips/search?key=AIzaSyAoBexp2doZWkhqk1nNKby3KfXIa737dMs"//AIzaSyDlW31JmWnRfy96JfYhjDQiL2ZQNYB2xkk";
+
+
+
+        var queryURL = "https://cors-anywhere.herokuapp.com/https://www.googleapis.com/qpxExpress/v1/trips/search?key=AIzaSyBao5t2cXEN-W6a_Mw0JBIUlifRXiSaLaM"  
+        //AIzaSyAoBexp2doZWkhqk1nNKby3KfXIa737dMs"//AIzaSyDlW31JmWnRfy96JfYhjDQiL2ZQNYB2xkk";
 
         $.ajax({
         url: queryURL,
         headers: {"Content-Type":"application/json"},
         data: JSON.stringify(params),
         method: "POST"
+
+
+        
+
         }).done((response) => {
          console.log(response);
 
@@ -111,7 +96,7 @@ function flightSearch(){
          var flightPrice6 = response.trips.tripOption[5].saleTotal + "<br>" + "Flight Number: " + response.trips.tripOption[5].slice[0].segment[0].flight.number + "<br>" + "Airline: " + response.trips.tripOption[5].slice[0].segment[0].flight.carrier + "<br>" + fromFlight + " => " + airCode;
          var flightLink = "https://www.google.com/flights/#search;f="+fromFlight+";t="+airCode+";d="+date+";r="+fReturn;
          //console.log(flightPrice1);
-         $(".bFlight").append("<a href="+flightLink+"target=_blank>Click to Purchase Flights!</a>");
+         $(".bFlight").append("<a href="+flightLink+" "+"target='_blank'" +">Click to Purchase Flights!</a>");
          $(".flight1").append("Total Price: $" + flightPrice1.slice(3));
          $(".flight2").append("Total Price: $" + flightPrice2.slice(3));
          $(".flight3").append("Total Price: $" + flightPrice3.slice(3));
@@ -136,151 +121,144 @@ function flightSearch(){
 function airportCode(){
 
   var queryURL = "https://cors-anywhere.herokuapp.com/http://www.distance24.org/route.json?stops="+zipCode
+  
   $.ajax({
     url: queryURL,
     method: "GET"
   }).done(function(response){
     console.log(response);
-    airCode = response.stops[0].airports[0].iata;
+   airCode = response.stops[0].airports[0].iata;
     params.request.slice[0].destination = airCode;
     returnFlight2 = response.stops[0].airports[0].iata;
-    params.request.slice[1].origin = returnFlight2; 
+    params.request.slice[1].origin = returnFlight2;
     
-    console.log("new Dest " + airCode);
+   //console.log("new Dest " + airCode);
     console.log("help " + returnFlight2);
 
-  });
+ });
 
+ 
   var URL = "https://cors-anywhere.herokuapp.com/http://www.distance24.org/route.json?stops="+zips
+  
   $.ajax({
     url: URL,
     method: "GET"
   }).done(function(response){ 
     console.log(response);
     fromFlight = response.stops[0].airports[0].iata;
-    params.request.slice[0].origin = fromFlight;
+    fromFlight = params.request.slice[0].origin;
+
+
 
     returnFlight1 = response.stops[0].airports[0].iata;
-    params.request.slice[1].destination = returnFlight1;
+    returnFlight = params.request.slice[1].destination;
 
-    console.log("from " + fromFlight);
-    console.log("way back " + returnFlight1);
-
+    // console.log("from " + fromFlight);
+    // console.log("way back " + returnFlight1);
 
   });
 
-   setTimeout(function() { flightSearch(); }, 1500);
-  
-    console.log(params);
-    
+  setTimeout(function() { flightSearch(); }, 1500);
+  // console.log(params); 
 };
 
 
 var map, infoWindow;
-      function initMap() {
-       
-        if (navigator.geolocation) {
-          navigator.geolocation.getCurrentPosition(function(position) {
-            var pos = {
-              lat: position.coords.latitude,
-              lng: position.coords.longitude
-            };
-            var lat = pos.lat;
-            var long = pos.lng;
-            var queryURL ="https://cors-anywhere.herokuapp.com/https://maps.googleapis.com/maps/api/geocode/json?latlng="+lat+","+long+"&key=AIzaSyBao5t2cXEN-W6a_Mw0JBIUlifRXiSaLaM";
-            $.ajax({
-            url: queryURL,
-            method: "GET"
-            }).done(function(response){
-            console.log(response);
-            console.log(lat);
-            console.log(long);
-            zips = response.results[0].address_components[6].long_name;
-            console.log("hey " + zips);   
-      });
+  function initMap() {
+    if (navigator.geolocation) {
+      navigator.geolocation.getCurrentPosition(function(position) {
+        var pos = {
+          lat: position.coords.latitude,
+          lng: position.coords.longitude
         };
-      };
+        var lat = pos.lat;
+        var long = pos.lng;
+        var queryURL ="https://cors-anywhere.herokuapp.com/https://maps.googleapis.com/maps/api/geocode/json?latlng="+lat+","+long+"&key=AIzaSyBao5t2cXEN-W6a_Mw0JBIUlifRXiSaLaM";
+        
+        $.ajax({
+          url: queryURL,
+          method: "GET"
+        }).done(function(response){
+          // console.log(response);
+          // console.log(lat);
+          // console.log(long);
+          zips = response.results[0].address_components[8].long_name;
+          // console.log("hey " + zips);   
+      });
+    });
+  };
+};
+
+initMap();
+
 $("#search-button").on("click", function(){
   artist = $("#search-input").val().trim();
- $("#artistSpace").empty();
-  var queryURL = "https://cors-anywhere.herokuapp.com/https://api.seatgeek.com/2/events?q=" + artist + "&per_page=1&client_id=MTAyMzg3N3wxNDk4MDEzODgyLjUy";
+    $("#artistSpace").empty();
+    var queryURL = "https://cors-anywhere.herokuapp.com/https://api.seatgeek.com/2/events?q=" + artist + "&per_page=1&client_id=MTAyMzg3N3wxNDk4MDEzODgyLjUy";
 
-
-
-  if(!artist) {
-    return false
-  }
-
-
-
-  
-
-  $.ajax({
-    url: queryURL,
-    method: 'GET'
-  }).done(function(response) {
-    console.log(response);
-
-//If there are no events coming up for the artist the following happens
-
-    if(response.events[0] === undefined){
-      
-      $("#first-page").empty();
-      $("#noArtist").text("Sorry " + artist + " is not performing anytime soon... Try another artist.");
-      var queryURL = "https://cors-anywhere.herokuapp.com/https://api.giphy.com/v1/gifs/search?q=sorry-taylor-swift&rating=pg-13&api_key=dc6zaTOxFJmzC";
+    if(!artist) {
+      return false
+    }
 
     $.ajax({
       url: queryURL,
       method: 'GET'
     }).done(function(response) {
-        var newDiv = $("<div>")
-        var artistGif = $("<img>");
-        artistGif.addClass("col s6 offset-s3");
-        artistGif.attr("src", response.data[1].images.fixed_height.url);
-        $("#noArtist").append(newDiv);
-        newDiv.append(artistGif);
-        
-      
+      // console.log(response);
 
- });
+      //If there are no events coming up for the artist the following happens
+      if(response.events[0] === undefined){   
+        $("#first-page").empty();
+        $("#noArtist").text("Sorry " + artist + " is not performing anytime soon... Try another artist.");
+        var queryURL = "https://cors-anywhere.herokuapp.com/https://api.giphy.com/v1/gifs/search?q=sorry-taylor-swift&rating=pg-13&api_key=dc6zaTOxFJmzC";
 
-    }
+        $.ajax({
+          url: queryURL,
+          method: 'GET'
+        }).done(function(response) {
+            var newDiv = $("<div>")
+            var artistGif = $("<img>");
+            artistGif.addClass("col s6 offset-s3");
+            artistGif.attr("src", response.data[1].images.fixed_height.url);
+            $("#noArtist").append(newDiv);
+            newDiv.append(artistGif);
+        });
+        } else {
+          $("#noArtist").empty();
 
-    else {
-    $("#noArtist").empty();
-    hotelArea = response.events[0].venue.display_location;
-    areaLocation = response.events[0].venue.city;
-    venueName = response.events[0].venue.name;
-    console.log(venueName);
-    website = response.events[0].url;
-    var upcomingEvents = response.events[0].has_upcoming_events;
-    zipCode = response.events[0].venue.postal_code;
-    console.log(zipCode);
-    console.log(hotelArea);
-    date = moment(response.events[0].datetime_local).subtract(1, "days").format('YYYY-MM-DD');
-    params.request.slice[0].date = date;
-    fReturn = moment(response.events[0].datetime_local).add(1, "days").format('YYYY-MM-DD');
-    params.request.slice[1].date = fReturn;
-  console.log(date)
+          var upcomingEvents = response.events[0].has_upcoming_events;
 
-  airportCode();
-  startSearch();
+          venueLatitude = response.events[0].venue.location.lat;
+          venueLongitude = response.events[0].venue.location.lon;
+          hotelArea = response.events[0].venue.display_location;
+          areaLocation = response.events[0].venue.city;
+          venueName = response.events[0].venue.name;
+          website = response.events[0].url;
+          zipCode = response.events[0].venue.postal_code;
+          date = moment(response.events[0].datetime_local).subtract(1, "days").format('YYYY-MM-DD');
+          params.request.slice[0].date = date;
+          fReturn = moment(response.events[0].datetime_local).add(1, "days").format('YYYY-MM-DD');
+          params.request.slice[1].date = fReturn;
+          
+          airportCode();
+          startSearch();
+          getGif();
+          placeSearch();
+          
+          // console.log(venueName);
+          // console.log(zipCode);
+          // console.log("hotel area: " + hotelArea);
+          // console.log(date)
+          // console.log(artist);
 
-  getGif();
-
-  console.log(artist);
-  $("#search-input").val("");
-  }
+          $("#search-input").val("");
+        }
   });
-
 }); 
-
-}); 
-
 
 
 function placeSearch() {
-  var queryURL = "https://cors-anywhere.herokuapp.com/https://maps.googleapis.com/maps/api/place/nearbysearch/json?location=-33.8670,151.1957&radius=500&types=food&name=cruise&key=AIzaSyDXrEeiKlrfaQDsH61Sk7OK5xCfJcg8J1M";
+  var queryURL = "https://cors-anywhere.herokuapp.com/https://maps.googleapis.com/maps/api/place/nearbysearch/json?location=" + venueLatitude + "," + venueLongitude + "&radius=500&types=hotel&key=AIzaSyDXrEeiKlrfaQDsH61Sk7OK5xCfJcg8J1M";
   
   $.ajax({ 
     url: queryURL,
@@ -291,34 +269,35 @@ function placeSearch() {
 };
 
 
-placeSearch();
-
 
 function getGif(){
   console.log("called");
   
-      var queryURL = "https://cors-anywhere.herokuapp.com/https://api.giphy.com/v1/gifs/search?q=" + artist + "&rating=pg-13&api_key=dc6zaTOxFJmzC";
+   var queryURL = "https://cors-anywhere.herokuapp.com/https://api.giphy.com/v1/gifs/search?q=" + artist + "&rating=pg-13&api_key=dc6zaTOxFJmzC";
 
-    $.ajax({
-      url: queryURL,
-      method: 'GET'
-    }).done(function(response) {
-        var newDiv = $("<div>")
-        var artistGif = $("<img>");
-        artistGif.addClass("col s6 offset-s3");
-        artistGif.attr("src", response.data[0].images.fixed_height.url);
-        moveGif = response.data[0].images.fixed_height.url;
-        stillGif = response.data[0].images.fixed_height_still.url;
-        $("#artistSpace").html("<h2> Sweet! " + artist + " will be performing soon on " + date + " in " + areaLocation + " at the " + venueName + "<a href=" + website + " " + "target='_blank'" + "> Click here to purchase tickets.</a></h2>");
-        $("#artistSpace").append(newDiv);
-        newDiv.append(artistGif);
-        
-      
+  $.ajax({
+    url: queryURL,
+    method: 'GET'
+  }).done(function(response) {
+    var newDiv = $("<div>")
+    var artistGif = $("<img>");
 
+    artistGif.addClass("col s6 offset-s3");
+    artistGif.attr("src", response.data[0].images.fixed_height.url);
+    moveGif = response.data[0].images.fixed_height.url;
+    stillGif = response.data[0].images.fixed_height_still.url;
+
+    $("#artistSpace").html("<h2> Sweet! " + artist + " will be performing soon on " + date + " in " + areaLocation + " at the " + venueName + "<a href=" + website + " " + "target='_blank'" + "> Click here to purchase tickets.</a></h2>");
+    $("#artistSpace").append(newDiv);
+
+    newDiv.append(artistGif);
  });
 };
+
+
 function dadJoke() {
   var queryURL ="https://icanhazdadjoke.com/slack";
+
   $.ajax({
     url: queryURL,
     method: 'GET'
@@ -327,22 +306,20 @@ function dadJoke() {
     $("#dadJoke").text(joke);
 
   });
-}
+};
+
 function loading() {
   dadJoke();
   $("#whole").hide();
   $("#loadingScreen").show();
   time();
-}
+};
 
 function time(){
   t = setTimeout(clearout, 6000)
-}
+};
+
 function clearout(){
   $("#loadingScreen").hide();
   $("#whole").show();
-
-}
-
-
-
+};
